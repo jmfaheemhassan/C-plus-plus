e2747b0c31d7af1f24ca455415dae877a7ce407d
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-02-14 01:15:30 +0600

    chapter 2 exercies 3

---
+++ c2e3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+void function_1() 
+{std::cout << "Three blind mice" <<std::endl;}
+void function_2()
+{std::cout << "see how they run" <<std::endl;}
+
+int main(){
+    function_1();
+    function_1();
+    function_2();
+    function_2();
+
+
+    return 0;
+}
